5d6bde96c163ed8b712b6f3677e9c152c4cbedb6
Connie Park <conniedc1206@gmail.com>
2022-07-20 09:32:40 -0700

    commit tree d89eba5442aa1cc9b2e0b9aab0b5206853130bb7

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,9 @@
 class ItemsController < ApplicationController
+    # GET /items
+    def index
+        # define the items
+        # render json to include user
+        items = Item.all
+        render json: items, include: :user
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+    # GET /users/:id
+    def show
+        # find the user
+        # return the json of the user object, including the items object nested
+        user = User.find_by(params[:id])
+        render json: user, include: :items
+    end
 end
